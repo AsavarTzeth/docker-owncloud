73234005060ddf2c381a80264dc23c77c09c49a9
Patrik Nilsson <asavartzeth@gmail.com>
2014-08-11 22:43:38 +0200

    first functional commit, cleanup, error fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,7 +17,7 @@ if [ "$OWNCLOUD_DB_TYPE" != 'sqlite' ]; then
 	# If database link of type "pgsql" is missing, exit. Also catches spelling errors.
 	if [ -z "$POSTGRES_PORT_5432_TCP" -a "$OWNCLOUD_DB_TYPE" != 'mysql' ]; then
 		echo >&2 'error: missing MYSQL_PORT_3306_TCP environment variable'
-		echo >&2 ' Did you forget to --link some_mysql_container:mysql ?'
+		echo >&2 ' Did you forget to --link some_postgres_container:postgres ?'
 		echo >&2 ' Or did you mistype "pgsql" ?'
 		exit 1
 	fi
@@ -44,25 +44,31 @@ if [ "$OWNCLOUD_DB_TYPE" != 'sqlite' ]; then
 	fi
 fi
 
-: ${PHP5_FPM_MEMORY_LIMIT:=128M}
-: ${PHP5_FPM_LOG_LEVEL:=notice}
-: ${PHP5_FPM_LISTEN:=127.0.0.1:9000}
+# TODO Detect if sql container is linked and override $OWNCLOUD_DB_TYPE accordingly, if still set to sqlite
 
-# TODO Implement UNIX socket setting in MariaDB container and maybe make pull request to MySQL, after testing that is.
-
-# If we're linked to PHP5-FPM, transfer settings so they are the same
+# If we're linked to PHP5-FPM, get settings from there, else set defaults locally.
 if [ -n "$PHP5_FPM_PORT_9000_TCP" ]; then
     : ${PHP5_FPM_MEMORY_LIMIT:=$PHP5_FPM_ENV_PHP5_FPM_MEMORY_LIMIT}
     : ${PHP5_FPM_LOG_LEVEL:=$PHP5_FPM_ENV_PHP5_FPM_LOG_LEVEL}
-    : ${PHP5_FPM_MEMORY_LISTEN:=$PHP5_FPM_ENV_PHP5_FPM_LISTEN}
+    : ${PHP5_FPM_LISTEN:=$PHP5_FPM_ENV_PHP5_FPM_LISTEN}
+elif
+    : ${PHP5_FPM_MEMORY_LIMIT:=128M}
+    : ${PHP5_FPM_LOG_LEVEL:=notice}
+    : ${PHP5_FPM_LISTEN:=127.0.0.1:9000}
 fi
 
-# If no SSL certificate
+# TODO check value "$PHP5_FPM_LISTEN" in case unix socket is used. If it is edit nginx.conf accordingly.
+
+: ${OWNCLOUD_SSL_CERT:=/etc/ssl/nginx/ssl.crt}
+: ${OWNCLOUD_SSL_CERT_KEY:=/etc/ssl/nginx/ssl.key}
+
+# If no SSL certificate exists generate a self-signed one.
 if [ -f "$SSL_DIR/ssl.crt" -a -f "$SSL_DIR/ssl.key" ]; then
-    
+    openssl req -x509 -newkey rsa:2048 -keyout $SSL_DIR/ssl.key -out $SSL_DIR/ssl.crt -nodes -days XXX
+    chmod 600 $SSL_DIR/ssl.*
 fi
 
-# TODO If no ssl cert/key is supplied autogenerate one.
+# TODO handle and use a CA, preferably located outside container, so certificates can be revoked.
 
 if ! [ -e index.php -a -e version.php ]; then
 	echo >&2 "ownCloud not found in $WEB_ROOT - copying now..."
@@ -73,13 +79,15 @@ if ! [ -e index.php -a -e version.php ]; then
 	echo >&2 "Complete! ownCloud has been successfully copied to $WEB_ROOT"
 fi
 
-# TODO handle WordPress upgrades magically in the same way, but only if version.php's $OC_VersionString is less than /usr/src/owncloud/version.php's $OC_VersionString
+# TODO handle ownCloud upgrades magically in the same way, but only if version.php's $OC_VersionString is less than /usr/src/owncloud/version.php's $OC_VersionString
 
 # If $CONF_DIR/nginx.conf is missing, copy from /usr/src/owncloud
 if ! [ -e $CONF_NGINX/nginx.conf ]; then
     echo >&2 "nginx.conf not found in $CONF_NGINX - copying now..."
     cp -a $SRC_DIR/nginx.conf $CONF_NGINX/
 fi
+
+# TODO Optional installation and config of php5 cache.
 
 : ${OWNCLOUD_DOMAIN_NAME:=localhost}
 : ${OWNCLOUD_FORCE_SSL:=true}
@@ -89,27 +97,25 @@ set_config() {
     key="$1"
     value="$2"
     if [ "$config_file" = "$CONF_NGINX/nginx.conf" ]; then
-        sed -ri "s|\S*($key\s+).*|\1 $value|g" $config_file
-    fi
-    if [ "$config_file" = "$CONF_PHP5" ]; then
+        sed -ri "s|\S*($key)\s+[^;]*|\1 $value|g" $config_file
+    elif [ "$config_file" = "$CONF_PHP5" ]; then
         for i in $(find $CONF_PHP5 -type f); do
             sed -ri "s|\S*($key\s+=).*|\1 $value|g" $i
         done
-    fi
-    if [ "$config_file" = "$CONF_OWNCLOUD/config.php" ]; then
-        sed -ri "s|\S*($key\s+).*|\1 $value|g" $config_file
+    elif [ "$config_file" = "$CONF_OWNCLOUD/config.php" ]; then
+        sed -ri "s|\S*($key\S+ =>)[^,]*|\1 \"$value\"|g" $config_file
     fi
 }
-
-config_file="$CONF_PHP5"
-set_config 'memory_limit' "$PHP5_FPM_MEMORY_LIMIT"
-set_config 'log_level' "$PHP5_FPM_LOG_LEVEL"
-set_config 'listen' "$PHP5_FPM_LISTEN"
 
 config_file="$CONF_NGINX/nginx.conf"
 set_config 'server_name' "$OWNCLOUD_DOMAIN_NAME"
 set_config 'ssl_certificate' "$OWNCLOUD_SSL_CERT"
 set_config 'ssl_certificate_key' "$OWNCLOUD_SSL_CERT_KEY"
+
+config_file="$CONF_PHP5"
+set_config 'memory_limit' "$PHP5_FPM_MEMORY_LIMIT"
+set_config 'log_level' "$PHP5_FPM_LOG_LEVEL"
+set_config 'listen' "$PHP5_FPM_LISTEN"
 
 config_file="$CONF_OWNCLOUD/config.php"
 set_config 'dbtype' "$OWNCLOUD_DB_TYPE"
@@ -140,14 +146,6 @@ if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_strin
 $mysql->close();
 EOPHP
 
-# FIX: Removed memcached as an option, since it should just be set to local anyways.
-# So to implement this the default setting just has to be in config.php (maybe check for link, if needed)
-
-# FIX: Just like wordpess check "$PHP5_FPM_LISTEN" for optional unix socket setting.
-# In any case setting should be taken from there and fall back to default. No user entry required.
-
-# IDEA: Detect if sql container is linked and override $OWNCLOUD_DB_TYPE accordingly
-
-#opt_install # IDEA: Optional installation and config of php5 cache. Check wordpress entrypoint again.
+# TODO Implement UNIX socket setting in MariaDB container and maybe make pull request to MySQL, after testing that is. This TODO really belongs there, not here.
 
 exec "$@"

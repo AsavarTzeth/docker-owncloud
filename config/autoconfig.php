00fe265a1dcb44aa1b886d58dab13776002cc0c7
Patrik Nilsson <asavartzeth@gmail.com>
2014-08-11 13:08:00 +0200

    started huge rewrite of script (not runnable atm)

---
+++ config/autoconfig.php
@@ -0,0 +1,11 @@
+<?php
+$AUTOCONFIG = array(
+"dbtype" => "sqlite",
+"dbname" => "owncloud",
+"dbuser" => "root",
+"dbpass" => "",
+"dbhost" => "localhost",
+"dbtableprefix" => "oc",
+"adminlogin" => "oc_admin",
+"adminpass" => "",
+);

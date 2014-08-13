1021aa8c09266f6dab91132d558611b03bd8e54c
Patrik Nilsson <asavartzeth@gmail.com>
2014-08-13 13:12:26 +0200

    bunch of bug fixes & commented php script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -47,22 +47,20 @@ fi
 # TODO Detect if sql container is linked and override $OWNCLOUD_DB_TYPE accordingly, if still set to sqlite
 
 # If we're linked to PHP5-FPM, get settings from there, else set defaults locally.
+: ${PHP5_FPM_MEMORY_LIMIT:=128M}
+: ${PHP5_FPM_LOG_LEVEL:=notice}
+: ${PHP5_FPM_LISTEN:=127.0.0.1:9000}
 if [ -n "$PHP5_FPM_PORT_9000_TCP" ]; then
     : ${PHP5_FPM_MEMORY_LIMIT:=$PHP5_FPM_ENV_PHP5_FPM_MEMORY_LIMIT}
     : ${PHP5_FPM_LOG_LEVEL:=$PHP5_FPM_ENV_PHP5_FPM_LOG_LEVEL}
     : ${PHP5_FPM_LISTEN:=$PHP5_FPM_ENV_PHP5_FPM_LISTEN}
-elif
-    : ${PHP5_FPM_MEMORY_LIMIT:=128M}
-    : ${PHP5_FPM_LOG_LEVEL:=notice}
-    : ${PHP5_FPM_LISTEN:=127.0.0.1:9000}
 fi
 
 # TODO check value "$PHP5_FPM_LISTEN" in case unix socket is used. If it is edit nginx.conf accordingly.
 
+# If no SSL certificate exists generate a self-signed one.
 : ${OWNCLOUD_SSL_CERT:=/etc/ssl/nginx/ssl.crt}
 : ${OWNCLOUD_SSL_CERT_KEY:=/etc/ssl/nginx/ssl.key}
-
-# If no SSL certificate exists generate a self-signed one.
 if [ -f "$SSL_DIR/ssl.crt" -a -f "$SSL_DIR/ssl.key" ]; then
     openssl req -x509 -newkey rsa:2048 -keyout $SSL_DIR/ssl.key -out $SSL_DIR/ssl.crt -nodes -days XXX
     chmod 600 $SSL_DIR/ssl.*
@@ -71,12 +69,12 @@ fi
 # TODO handle and use a CA, preferably located outside container, so certificates can be revoked.
 
 if ! [ -e index.php -a -e version.php ]; then
-	echo >&2 "ownCloud not found in $WEB_ROOT - copying now..."
-	rsync --archive --one-file-system --quiet --exclude='*.sh' --exclude='*.conf' --exclude='*.bz2*' --exclude='Dockerfile' $SRC_DIR $WEB_ROOT
-	chown -R www-data:www-data $WEB_DIR \
-        find "$WEB_ROOT" -type d -exec chmod 750 {} \;
-        find "$WEB_ROOT" -type f -exec chmod 640 {} \;
-	echo >&2 "Complete! ownCloud has been successfully copied to $WEB_ROOT"
+    echo >&2 "owncloud not found in $WEB_ROOT - copying now..."
+    rsync --archive --one-file-system --quiet --exclude='*.sh' --exclude='*.conf' --exclude='*.bz2*' --exclude='Dockerfile' $SRC_DIR $WEB_ROOT/..
+    chown -R www-data:www-data $WEB_ROOT
+    find "$WEB_ROOT" -type d -exec chmod 750 {} \;
+    find "$WEB_ROOT" -type f -exec chmod 640 {} \;
+    echo >&2 "Complete! owncloud has been successfully copied to $WEB_ROOT"
 fi
 
 # TODO handle ownCloud upgrades magically in the same way, but only if version.php's $OC_VersionString is less than /usr/src/owncloud/version.php's $OC_VersionString
@@ -125,26 +123,26 @@ set_config 'dbname' "$OWNCLOUD_DB_NAME"
 set_config 'forcessl' "$OWNCLOUD_FORCE_SSL"
 set_config 'loglevel' "$OWNCLOUD_LOG_LEVEL"
 
-TERM=dumb php -- "$OWNCLOUD_DB_HOST" "$OWNCLOUD_DB_USER" "$OWNCLOUD_DB_PASSWORD" "$OWNCLOUD_DB_NAME" <<'EOPHP'
-<?php
-// database might not exist, so let's try creating it (just to be safe)
+#TERM=dumb php -- "$OWNCLOUD_DB_HOST" "$OWNCLOUD_DB_USER" "$OWNCLOUD_DB_PASSWORD" "$OWNCLOUD_DB_NAME" <<'EOPHP'
+#<?php
+#// database might not exist, so let's try creating it (just to be safe)
 
-list($host, $port) = explode(':', $argv[1], 2);
-$mysql = new mysqli($host, $argv[2], $argv[3], '', (int)$port);
+#list($host, $port) = explode(':', $argv[1], 2);
+#$mysql = new mysqli($host, $argv[2], $argv[3], '', (int)$port);
 
-if ($mysql->connect_error) {
-	file_put_contents('php://stderr', 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
-	exit(1);
-}
+#if ($mysql->connect_error) {
+#	file_put_contents('php://stderr', 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
+#	exit(1);
+#}
 
-if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($argv[4]) . '`')) {
-	file_put_contents('php://stderr', 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
-	$mysql->close();
-	exit(1);
-}
+#if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($argv[4]) . '`')) {
+#	file_put_contents('php://stderr', 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
+#	$mysql->close();
+#	exit(1);
+#}
 
-$mysql->close();
-EOPHP
+#$mysql->close();
+#EOPHP
 
 # TODO Implement UNIX socket setting in MariaDB container and maybe make pull request to MySQL, after testing that is. This TODO really belongs there, not here.
 

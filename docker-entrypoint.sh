0267c3f44c0264a8eb9aa389c504ec6dfca9bb27
Patrik Nilsson <asavartzeth@gmail.com>
2014-08-25 15:26:43 +0200

    added expect to openssl

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -62,7 +62,20 @@ fi
 : ${OWNCLOUD_SSL_CERT:=$SSL_DIR/ssl.crt}
 : ${OWNCLOUD_SSL_CERT_KEY:=$SSL_DIR/ssl.key}
 if ! [ -f "$SSL_DIR/ssl.crt" -a -f "$SSL_DIR/ssl.key" ]; then
-    openssl req -x509 -newkey rsa:2048 -keyout $SSL_DIR/ssl.key -out $SSL_DIR/ssl.crt -nodes -days XXX
+    expect << EOF
+	set timeout 600
+	set send_slow {1 1}
+	openssl req -x509 -newkey rsa:2048 -keyout $SSL_DIR/ssl.key -out $SSL_DIR/ssl.crt -nodes -days XXX
+	expect {
+	    "Country Name (2 letter code) [AU]:" { exp_continue }
+	    "State or Province Name (full name) [Some-State]:" { exp_continue }
+	    "Locality Name (eg, city) []:" { exp_continue }
+	    "Organization Name (eg, company) [Internet Widgits Pty Ltd]:" { exp_continue }
+	    "Organizational Unit Name (eg, section) []:" { exp_continue }
+	    "Common Name (e.g. server FQDN or YOUR name) []:" { sleep 1; send "docker-owncloud"; exp_continue }
+	    "Email Address []:" { exp_continue }
+	}
+EOF
     chmod 600 $SSL_DIR/ssl.*
 fi
 
@@ -79,10 +92,11 @@ fi
 
 # TODO handle ownCloud upgrades magically in the same way, but only if version.php's $OC_VersionString is less than /usr/src/owncloud/version.php's $OC_VersionString
 
-# If $CONF_DIR/nginx.conf is missing, copy from /usr/src/owncloud
+# If $CONF_DIR/nginx.conf is missing, copy from $SRC_DIR
 if ! [ -e $CONF_NGINX/nginx.conf ]; then
     echo >&2 "nginx.conf not found in $CONF_NGINX - copying now..."
-    cp -a $SRC_DIR/nginx.conf $CONF_NGINX/
+    cp -a $SRC_DIR/nginx.conf $CONF_NGINX
+    echo >&2 "Complete! nginx.conf has been successfully copied to $CONF_NGINX"
 fi
 
 # TODO Optional installation and config of php5 cache.
